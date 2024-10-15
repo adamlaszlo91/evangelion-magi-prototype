a8905be13a0bf70f16c3503c8699cac7006bfb0d
Ádám László <adam.laszlo.91@gmail.com>
2024-10-15 13:58:53 +0200

    Add Ollama and LLM communication

--- main.py
+++ main.py
@@ -1,33 +1,38 @@
 from ascii_logo import ascii_logo
 from threading import Thread
+import ollama
 
-"""
-Magi-1: Melchior: Dr. Naoko Akagi as a scientist;
-Magi-2: Balthasar: Dr. Naoko Akagi as a mother;
-Magi-3: Casper: Dr. Naoko Akagi as a woman.
-"""
+system_prompt = 'A decision making request has been made: \"{}\"\nThree proposals have been made:\n\"{}\"\n\"{}\"\n\"{}\"\nCreate a final proposal based on thre previous ones! Answer in one sentence maximum.'
+subsystem_prompt = 'Act as a decision making subsystem (part of Magi from Neon Genesis Evangelion.). You propose decisions from the point of view of a {}. Answer in one sentence maximum.'
 answers = [None, None, None]
 
 
 def acquire_melchior_response(input: str) -> None:
-    response = 'yo'
+    answers[0] = None
+    response = ollama.chat(model='llama3.1', messages=[{'role': 'system', 'content': subsystem_prompt.format('scientist')},
+                                                       {'role': 'user', 'content': input}],)['message']['content']
     answers[0] = response
     print(f'Melchior: {response}')
 
 
 def acquire_balthasar_response(input: str) -> None:
-    response = 'yo'
+    answers[1] = None
+    response = ollama.chat(model='llama3.1', messages=[{'role': 'system', 'content': subsystem_prompt.format('mother')},
+                                                       {'role': 'user', 'content': input}],)['message']['content']
     answers[1] = response
     print(f'Balthasar: {response}')
 
 
 def acquire_casper_response(input: str) -> None:
-    response = 'yo'
+    answers[2] = None
+    response = ollama.chat(model='llama3.1', messages=[{'role': 'system', 'content': subsystem_prompt.format('woman')},
+                                                       {'role': 'user', 'content': input}],)['message']['content']
     answers[2] = response
     print(f'Casper: {response}')
 
 
 def collect_responses(input: str) -> str:
+    print()
     thread1 = Thread(target=acquire_melchior_response, args=[input])
     thread1.start()
     thread2 = Thread(target=acquire_balthasar_response, args=[input])
@@ -39,7 +44,11 @@ def collect_responses(input: str) -> str:
     thread2.join()
     thread3.join()
 
-    return '\n'.join(answers)
+    print()
+    print('Suggestion:')
+    response = ollama.chat(model='llama3.1', messages=[{'role': 'system', 'content': system_prompt.format(input, answers[0], answers[1], answers[2])},
+                                                       {'role': 'user', 'content': '\n\n'.join(answers)}],)['message']['content']
+    return response
 
 
 def main() -> None:
